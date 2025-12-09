a692896d80234f2dcc786e86fb32f6814f1e48da
calixto <joaopedro.calixto10@gmail.com>
2025-12-09 16:05:38 -0300

    PositionalEncoding do model.py

--- model.py
+++ model.py
@@ -8,11 +8,51 @@ class InputEmbeddings(nn.Module):
             d_model: int, # dimensão do modelo ()
             voc_size: int # qtd de palavras no meu vocabulário
     ):
+        super().__init__()
         self.d_model = d_model
         self.voc_size = voc_size
         self.embedding = nn.Embedding(voc_size, d_model)
 
     def forward(self, x):
         return self.embedding(x) * math.sqrt(seld.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(
+        self,
+        d_model: int,  # dimensão do modelo
+        seq_len: int, # tamanho máximo da sequência
+        dropout: float, # dropout para configurar o grau de overfitting
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # criar uma matriz no formato (seq_len, d_model)
+        PE = torch.zeros(seq_len, d_model)
+
+        # criar um vetor do tamanho da sequência
+        position = torch.arange(0, seq_len, dtype=float).unsqueeze(1) # -> shape (seq_len, 1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)) / d_model)
+
+        # aplicar seno para as posições pares e cosseno para as ímpares
+        PE[:, 0::2] = torch.sin(position * div_term)
+        PE[:, 1::2] = torch.cos(position * div_term)
+
+        PE = PE.unsqueeze(0) # (1, seq_len, d_model)
+
+        # gravar esse tensor no buffer do modelo
+        self.register_buffer('PE', PE)
+
+    def forward(self, x):
+        # aplicar a função para que calcula o positional encodding 
+        # para cada token da sequência
+        x = x + (
+            self.PE[:, :x.shape[1], :]
+        ).requires_grad_(False) # O PE vai ser fixo, não vai ser aprendido
+        # no processo de treinamento
+
+        return self.dropout(x)
+
 
 print("tá funcionando")
